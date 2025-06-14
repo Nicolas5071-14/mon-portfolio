8a92f337d43dc05dbda95cfe3e2a00a29fc02620
Nicolas5071-14 <ramahalefitra.abelson.nicolas@gmail.com>
2025-06-14 19:26:21 +0300

    go

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom';
 
 const CustomLink = ({ to, ...props }: { to: string } & React.ComponentProps<typeof Link>) => {
-  return <Link to={to} {...props} />;
+  return <Link to={to} {...props} />; // Laissez le Router gérer le préfixe
 };
 
 export default CustomLink;
